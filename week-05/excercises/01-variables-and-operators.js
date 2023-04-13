bd2156a0dbda04aa334f4d73ae7ea458d38e11a3
Daniel Ninfi <dbninfi@gmail.com>
2023-04-13 14:02:00 -0300

    BaSP-W05-M2023: templates created

---
+++ week-05/excercises/01-variables-and-operators.js
@@ -0,0 +1,21 @@
+// 1. Variables y Operadores
+
+console.log('--EXERCISE 1: VARIABLES AND OPERATORS');
+
+// a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+
+console.log('-Exercise 1.a:');
+
+console.log(result);
+
+// b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+console.log('-Exercise 1.b:');
+
+console.log(result);
+
+// c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+
+console.log('-Exercise 1.c:');
+
+console.log(result);
---
+++ week-05/excercises/06-function.js
@@ -0,0 +1,33 @@
+//6. Funciones
+
+console.log('--EXERCISE 6: FUNCTIONS');
+
+//a. Crear una función suma que reciba dos valores numéricos y retorne el resultado. Ejecutar la función y guardar el resultado en una variable, mostrando el valor de dicha variable en la consola del navegador.
+
+console.log('-Exercise 6.a:');
+
+console.log(result);
+
+//b. Copiar la función suma anterior y agregarle una validación para controlar si alguno de los parámetros no es un número; de no ser un número, mostrar un alert aclarando que uno de los parámetros tiene error y retornar el valor NaN como resultado.
+
+console.log('-Exercise 6.b:');
+
+console.log(result);
+
+//c. Crear una función “validateInteger” que reciba un número como parámetro y devuelva verdadero si es un número entero.
+
+console.log('-Exercise 6.c:');
+
+console.log(result);
+
+//d. Copiar y renombrar la función suma del ejercicio 6b) y agregarle una llamada a la función del ejercicio 6c. y que valide que los números sean enteros. En caso que haya decimales mostrar un alert con el error y retornar el número convertido a entero (redondeado).
+
+console.log('-Exercise 6.d:');
+
+console.log(result);
+
+//e. Convertir la validación del ejercicio 6d) en una función separada y llamarla dentro de una nueva función probando que todo siga funcionando igual que en el apartado anterior.
+
+console.log('-Exercise 6.e:');
+
+console.log(result);
