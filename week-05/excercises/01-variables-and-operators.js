ba0a569effd596d2ec5ad4879bdaa8db1ab6ab94
Daniel Ninfi <dbninfi@gmail.com>
2023-04-18 10:57:49 -0300

    BaSP-W05-M2023: Corrections week 4, order of selectors and properties in style sheets

--- week-05/excercises/01-variables-and-operators.js
+++ week-05/excercises/01-variables-and-operators.js
@@ -2,40 +2,36 @@
 
 console.log('--EXERCISE 1: VARIABLES AND OPERATORS');
 
-// a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+// a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en 
+//      una 3er variable.
 
 console.log('-Exercise 1.a:');
 
-var firstNumber = 10
-
-var secondNumber = 20
-
+var firstNumber = 10;
+var secondNumber = 20;
 var result1a = firstNumber + secondNumber;
 
-console.log(result1a);
+console.log('Result 1a: ' + result1a);
 
 // b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
 
 console.log('-Exercise 1.b:');
 
 var stringHi = 'Hi';
-
 var stringRadium = 'Radium';
-
 var result1b = stringHi + " " + stringRadium;
 
-console.log(result1b);
+console.log('Result 1b: ' + result1b);
 
-// c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+// c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el 
+//      resultado de la suma en una 3er variable (utilizar length).
 
 console.log('-Exercise 1.c:');
 
 var rosario ='Rosario';
 var montevideo = 'Montevideo';
-
 var lengthR = rosario.length;
 var lengthM = montevideo.length;
-
 var result1c = lengthR + lengthM;
 
-console.log('number of letters: ' + result1c);
+console.log('Result 1c: ' + result1c);
